d14bf3794250f1dea04ae38fb7544c3adf07a0ba
Paulica <paul_hanga@yahoo.com>
2020-01-08 22:28:43 +0200

    tema Java1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,28 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int a,b,c,d;
+        float a1, b1, c1;
+        System.out.println("Hello World! ");
+        System.out.println("Paul");
+        a=10;
+        b= 3;
+        c=a+b;
+        System.out.println("Suma dintre "+a+ " si "+b+" este: " +c);
+        d= a/b;
+        System.out.println(+a+ " impartit la "+b+" este: " +d+ " rest "+(a%b));
+        a1=10;
+        b1=3;
+        c1=a1/b1;
+        System.out.print("Pentru numere reale ");
+        System.out.println(+a1+ " impartit la "+b1+" este: " +c1);
+        System.out.print("-5 + 8 * 6 = ");
+        System.out.println(-5 + 8 * 6);
+        System.out.print("(55+9) % 9  = ");
+        System.out.println((55+9) % 9 );
+        System.out.print("20 + -3*5 / 8 = ");
+        System.out.println(20 + -3*5 / 8 );
+        System.out.print("5 + 15 / 3 * 2 - 8 % 3   = ");
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3  );
     }
 }
