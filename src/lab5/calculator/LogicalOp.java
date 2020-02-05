da3d9869745500636229513c2dd7c1a33cedb656
Paulica <paul_hanga@yahoo.com>
2020-02-05 21:45:37 +0200

    Tema 5 Arrays

---
+++ src/lab5/calculator/LogicalOp.java
@@ -0,0 +1,190 @@
+package lab5.calculator;
+
+public class LogicalOp {
+    public int[] arrayToTen() {
+        int[] array = new int[10];
+        for (int i = 1; i <= 10; i++) {
+            array[i - 1] = i;
+        }
+        return array;
+    }
+
+    public void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("a["+i+"]= "+array[i]+", ");
+        }
+        System.out.println();
+    }
+
+    public float averageOfArray(int[] array) {
+        float sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum/array.length;
+    }
+
+
+    public int adunare(int a, int b) {
+        int adunare = a + b;
+        return adunare;
+    }
+    public float adunare (float a, float b){
+        float adunare = a + b;
+        return adunare;
+    }
+    public int adunare(int a, int b, int c) {
+        int adunare = a + b + c;
+        return adunare;
+    }
+    public float adunare(float a, int b) {
+        float adunare = a + b;
+        return adunare;
+    }
+    public short scadere(short a, short b) {
+        short scadere = (short) (a - b);
+        return scadere;
+    }
+    public double scadere(double a, double b) {
+        double scadere = a - b;
+        return scadere;
+    }
+    public double scadere(int a, double b) {
+        double scadere = a - b;
+        return scadere;
+    }
+
+    public long inmultire(long a, long b) {
+        long inmultire = (long) (a * b);
+        return inmultire;
+    }
+    public int inmultire(int a, int b, int c) {
+        int inmultire = a * b*c;
+        return inmultire;
+    }
+    public float inmultire(float a, int b) {
+        float inmultire = a * b;
+        return inmultire;
+    }
+    public float inmultire(float a, float b) {
+        float inmultire = a * b;
+        return inmultire;
+    }
+
+    public double impartire(double a, double b) {
+        double impartire = a / b;
+        return impartire;
+    }
+    public int impartire(int a, int b) {
+        int impartire = a / b;
+        return impartire;
+    }
+    public double impartire(double a, double b,double c) {
+        double impartire = a / b/c;
+        return impartire;
+    }
+    public int[] arrayToHundred() {
+        int[] array = new int[100];
+        for (int i = 1; i <= 100; i++) {
+            array[i - 1] = i;
+        }
+        return array;
+    }
+    public int[] arrayToGivenLength(int n) {
+        int[] array = new int[n];
+        for (int i = 1; i <= n; i++) {
+            array[i - 1] = 0;
+        }
+        return array;
+    }
+    public int[] populare(int[] array){
+        for (int i = 1; i <=array.length; i++) {
+            array[i - 1] = 2*i;
+        }
+        return array;
+    }
+    public void printArray(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("a["+i+"]= "+array[i]+", ");
+        }
+        System.out.println();
+    }
+    public boolean findStringInArray (String[] array, String text){
+        boolean x=false;
+        for (int i=0; i<array.length; i++){
+            if (array[i].equals(text)) {
+                x= true;
+            }
+        }
+        return x;
+    }
+    public int verifyIfNumberINArray( int []array, int number){
+        int pozition= -1;
+        for (int i=0; i<array.length; i++){
+            if (array[i]==number) {
+                pozition= i;
+            }
+        }
+        return pozition;
+    }
+    public void afis_(){
+        String [][] a= new String[10][10];
+        for (int i=0; i<10;i++){
+            for (int j=0; j<10;j++){
+                System.out.print("_ ");
+            }
+            System.out.println();
+        }
+    }
+    public int[] removeNumberFromArray(int [] array, int number){
+        int j=0;
+        for (int i=0; i<array.length; i++){
+            if (array[i]!=number){
+                j++;
+            }
+        }
+        int [] mArray= new int[j];
+        j=0;
+        for (int i=0; i<array.length;i++) {
+            if (array[i] != number) {
+                mArray[j] = array[i];
+                j++;
+            }
+        }
+        return mArray;
+    }
+    public int secondSmallestNumber(int[] array){
+        int max=0;
+        for (int i=0; i<array.length;i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        int smallest=max;
+
+        if (array.length<=1){
+            System.out.println("The array is to short");
+        }
+        int secondSmallest=array[0];
+        for (int i=0; i<array.length;i++) {
+            if (array[i] < smallest) {
+                secondSmallest = smallest;
+                smallest = array[i];
+            } else if (array[i] < secondSmallest && array[i] != smallest) {
+                secondSmallest = array[i];
+            }
+        }
+        return secondSmallest;
+    }
+    public int [] copyArray (int [] array, int [] anotherArray){
+        if (array.length<anotherArray.length){
+            System.out.println("The length of the first array needs to be at least the size of the empty array");
+        }
+        for (int i=0; i<array.length; i++){
+            anotherArray[i]=array[i];
+
+        }
+        return anotherArray;
+    }
+}
+
---
+++ src/lab5/calculator/Main.java
@@ -0,0 +1,57 @@
+package lab5.calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] myArray=new int[10];
+        LogicalOp op = new LogicalOp();
+        int [] newArray =op.arrayToTen();
+        op.printArray(newArray);
+        System.out.println(op.averageOfArray(newArray));
+       int [] secondArray={2,8,7,5,8,5,9,99};
+        op.printArray(secondArray);
+        System.out.println("Adunarea cu float: "+op.adunare(5.6f, 6.4f));
+        System.out.println("Adunarea cu integer: "+op.adunare(5,6));
+        System.out.println("Adunara cu 3 numere intregi: "+op.adunare(2,4,7));
+        System.out.println("Adunarea cu un numat float si unu intreg: "+op.adunare(4.7f, 4));
+        System.out.println("Scaderea cu short: "+op.scadere((short)4, (short)-3));
+        System.out.println("Scaderea cu double: "+op.scadere(4.5, 2.5));
+        System.out.println("Scaderea cu un nr intreg si unu double: "+op.scadere(4, 4.5));
+        System.out.println("Inmultirea cu long: "+op.inmultire(2l,3l));
+        System.out.println("Inmultirea a trei numere intregi: "+op.inmultire(2,3,4));
+        System.out.println("Inmultirea cu double si intrg: " +op.inmultire(4.5f, 3));
+        System.out.println("Inmultirea cu float: "+op.inmultire(2.5f, 2.5f ));
+        System.out.println("Impartirea cu double: "+op.impartire(4d, 3d));
+        System.out.println("Impartirea cu nr intregi: "+op.impartire(4,2));
+        System.out.println("Impartirea cu 3 numere double: "+op.impartire(8d, 2d,7d));
+        int aArray[]= new int[100];
+        int oneHundredArray[]=op.arrayToHundred();
+        op.printArray(oneHundredArray);
+        int evenNumbersArray[]=op.arrayToGivenLength(50);
+        op.printArray(evenNumbersArray);
+        int evenNrArray[] = op.populare(evenNumbersArray);
+        op.printArray(evenNrArray);
+        System.out.println();
+        String[] nameArray={"Paul", "George", "Ionut","Catalin", "Ioana", "Elena"};
+        op.printArray(nameArray);
+        System.out.println("Teaxtul a fost gasit in lista: " +op.findStringInArray(nameArray, "elena"));
+        int x=(op.verifyIfNumberINArray(secondArray, 8));
+        if (x==-1){
+            System.out.println("Numarul nu fost gasit in sir");
+        }else System.out.println("Ultima pozitie pe care numarul a fost intalnit este: "+x);
+        x=(op.verifyIfNumberINArray(secondArray, 1));
+        if (x==-1){
+            System.out.println("Numarul nu fost gasit in sir");
+        }else System.out.println("Ultima pozitie pe care numarul a fost intalnit este: "+x);
+        op.afis_();
+        System.out.println("Sirul initial este: ");
+        op.printArray(secondArray);
+        op.printArray(op.removeNumberFromArray(secondArray, 8));
+        System.out.println("The second smallest number from the array is: "+op.secondSmallestNumber(secondArray));
+        System.out.println("The empty array");
+        int []arr =op.arrayToGivenLength(8);
+        op.printArray(arr);
+        System.out.println("The array after copy");
+        int bb[]=op.copyArray(secondArray, arr);
+        op.printArray(bb);
+    }
+}
